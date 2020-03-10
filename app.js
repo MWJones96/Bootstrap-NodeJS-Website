4a0f3c206fdd4289fde3b09c30cb4ed5954181c6
Matthew Jones <jonesmat@usc.edu>
2020-03-09 23:47:47 -0700

    XKCD comic added

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
-var quote = require('prog-quote')();
+var xkcd = require('xkcd');
+
+var currXKCD = null;
 
 var app = express();
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-	q = quote.next().value;
+    xkcd(function (data) {
+        currXKCD = data;
+    });
 	matt_age = ((new Date() - new Date(1996, 7, 8)) / (60 * 60 * 24 * 365 * 1000)).toFixed(2);
 
-    res.render('home', {quote: q.quote, author: q.author, matt_age_int: Math.floor(matt_age), matt_age: matt_age});
+    res.render('home', {matt_age_int: Math.floor(matt_age), matt_age: matt_age, xkcd: currXKCD});
 });
 
 app.get('/projects', function(req, res) {
